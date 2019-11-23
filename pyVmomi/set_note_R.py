c9e5e180478225ceb9f8889aecb191a4f8fefab5
anniesunqin <anniesunqin@sjtu.edu.cn>
2019-11-23 09:53:05 +0800

    Add files via upload

---
+++ pyVmomi/set_note_R.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# Copyright 2014 Michael Rice
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import atexit
+
+from pyVim import connect
+from pyVmomi import vim
+from tools import cli
+from tools import tasks
+
+
+def setup_args():
+    """
+    Adds additional args to allow the vm uuid to
+    be set.
+    """
+    parser = cli.build_arg_parser()
+    # using j here because -u is used for user
+    parser.add_argument('-i', '--ip',
+                        required=False,
+                        action='store',
+                        help='IP address of the VM to search for')
+    parser.add_argument('-m', '--message',
+                        required=True,
+                        help="Message to add to the notes field.")
+    my_args = parser.parse_args()
+    return cli.prompt_for_password(my_args)
+
+args = setup_args()
+si = None
+try:
+    si = connect.SmartConnectNoSSL(host=args.host,
+                                   user=args.user,
+                                   pwd=args.password,
+                                   port=int(args.port))
+    atexit.register(connect.Disconnect, si)
+    content = si.RetrieveContent()
+except IOError as e:
+    print(e)
+    pass
+
+if not si:
+    raise SystemExit("Unable to connect to host with supplied info.")
+
+vm = si.content.searchIndex.FindByIp(None, args.ip, True)
+if not vm:
+    raise SystemExit("Unable to locate VirtualMachine.")
+
+print("Found: {0}".format(vm.name))
+spec = vim.vm.ConfigSpec()
+spec.annotation = args.message
+task = vm.ReconfigVM_Task(spec)
+tasks.wait_for_tasks(si, [task])
+print("Done.")
