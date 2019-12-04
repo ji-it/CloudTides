047e84dac9c28fa67c9a088fcf4f0b5dc31f48da
Zhe Shen <48246860+Zhe-Shen@users.noreply.github.com>
2019-12-04 14:51:12 +0800

    add usage

--- server/resource/urls.py
+++ server/resource/urls.py
@@ -5,6 +5,6 @@ from .views import *
 urlpatterns = [
     path('validate/', ValidateResource.as_view(), name='validate'),
     path('add/', AddResource.as_view(), name='add'),
-    path('list/', ListResource.as_view(), name='list'),
+    #path('list/', ListResource.as_view(), name='list'),
     path('delete/', DeleteResource.as_view(), name='delete')
 ]
