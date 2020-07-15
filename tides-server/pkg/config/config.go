8a3b3a094b743ece05c8077924c62fbe7e5d6289
Wu Wentao <winter_wu@sjtu.edu.cn>
2020-07-15 14:59:43 +0800

    fix: CICD pipeline and DB connection

--- tides-server/pkg/config/config.go
+++ tides-server/pkg/config/config.go
@@ -40,8 +40,8 @@ func GetDB() *gorm.DB {
 }
 
 func startDB() {
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-		DB_USER, DB_PASSWORD, DB_NAME)
+	dbinfo := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
+		DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 	db, err = gorm.Open("postgres", dbinfo)
 	// defer db.Close()
 	if err != nil {
