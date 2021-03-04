8d55a7d04370f472df863ba3ee86ab13aed81b2e
Zhiyuan Xiang <xiangzhiyuan124@gmail.com>
2021-03-04 15:11:57 +0800

    fix: try to change home page to resource

--- ui/src/app/@shared/config/path.ts
+++ ui/src/app/@shared/config/path.ts
@@ -1,9 +1,9 @@
 // Path Name
 export const LOGIN_PATH_NAME = 'login';
 export const REGISTER_PATH_NAME = 'register';
-// export const HOME_PATH_NAME = 'home';
-export const HOME_PATH_NAME = 'resource';
-export const RESOURCE_PATH_NAME = 'resource';
+export const HOME_PATH_NAME = 'home';
+// export const RESOURCE_PATH_NAME = 'resource';
+export const RESOURCE_PATH_NAME = 'home';
 export const POLICY_PATH_NAME = 'policy';
 export const TEMPLATE_PATH_NAME = 'template';
 
@@ -20,7 +20,8 @@ export const PROFILE_API_URL = `/users/profile`;
 
 export const HOME_PATH = `/${HOME_PATH_NAME}`;
 
-export const RESOURCE_API_URL = `/resource`;
+//export const RESOURCE_API_URL = `/resource`;
+export const RESOURCE_API_URL = `/${RESOURCE_PATH_NAME}`
 export const VCD_URL_PATH = `${RESOURCE_API_URL}/vcd`;
 
 export const POLICY_PATH = `/${POLICY_PATH_NAME}`;
