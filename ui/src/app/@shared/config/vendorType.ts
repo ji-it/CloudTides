718f5e81d38ff4d15e2f67b0970721a039e15e12
Zhiyuan Xiang <xiangzhiyuan124@gmail.com>
2021-03-18 14:50:10 +0800

    fix: allow users to select the type rather than type it in

---
+++ ui/src/app/@shared/config/vendorType.ts
@@ -0,0 +1,6 @@
+export const vendorTypes = {
+    Fixed: 'Fixed',
+    Dynamic: 'Dynamic',
+  };
+  
+  export const defaultType = vendorTypes.Fixed;
--- ui/src/app/vendor/vendor-dialog/vendor-dialog.component.ts
+++ ui/src/app/vendor/vendor-dialog/vendor-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
+import { defaultType, vendorTypes } from '@tide-shared/config/vendorType';
 import { VendorService } from '../vendor.service';
 
 @Component({
@@ -18,9 +19,11 @@ export class VendorDialogComponent implements OnInit {
     this.vendorForm = this.fb.group({
       name: ['', Validators.required],
       url: ['', Validators.required],
-      vendorType: ['', Validators.required],
+      vendorType: [defaultType, Validators.required],
       version: [''],
     });
+    this.vendorTypeList = Object.keys(vendorTypes);
+    this.vendorTypes = vendorTypes;
   }
 
   @Input() opened = false;
@@ -28,6 +31,8 @@ export class VendorDialogComponent implements OnInit {
   @Output() cancel = new EventEmitter();
 
   vendorForm: FormGroup;
+  vendorTypeList: string[];
+  vendorTypes: any;
 
   readonly vo = {
     serverError: '',
